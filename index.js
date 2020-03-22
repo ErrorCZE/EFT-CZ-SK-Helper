2e8df6711557fdc50ba66dc9f94b6a6baef60056
ErrorCZE <47039176+ErrorCZE@users.noreply.github.com>
2020-03-22 08:20:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
 const bot = new Discord.Client();
 
 
+bot.on('ready', () => {
+  console.log('Ready!')
+});
+
+bot.on('message', message) => {
+if (message.content.startsWith(${prefix}help)) {
+  message.channel.send("Zatím toho moc neumím :)")
+});
 
 
 
